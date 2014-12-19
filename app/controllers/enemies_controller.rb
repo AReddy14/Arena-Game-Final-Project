3619d6efbd354c00e3712fea5d5f5db1418865be
areddy14 <areddy1441@gmail.com>
2014-12-19 00:02:35 -0500

    I believe that I've finished the create method for enemies.

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,4 +1,5 @@
 class EnemiesController < ApplicationController
+	before_action :index, only: [:create]
 	def index
 		# do I want an array of enemies?
 		@enemies = Enemy.all
@@ -8,11 +9,10 @@ class EnemiesController < ApplicationController
 		@enemies << Enemy.new
 		@enemies.last.type = "monster"
 		@enemies.last.level = player_level
-
 		@enemies.last.strength = 6 + (rand(1..3)*(player_level - 1))
 		@enemies.last.dexterity = 10 + (rand(1..3)*(player_level - 1))
 		@enemies.last.intelligence = 6 + (rand(1..3)*(player_level - 1))
-		@enemies.last.health = 6 + (rand(1..3)*(player_level - 1))
-		@enemies.last.mana = 6 + (rand(1..3)*(player_level - 1))
+		@enemies.last.health = 160 + (rand(10..20)*(player_level - 1))
+		@enemies.last.mana = 45 + (rand(3..5)*(player_level - 1))
 	end
 end
